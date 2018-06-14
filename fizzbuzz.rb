e5f3a3638947dfbcbdcbbd95c8668288139438f1
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-14 18:16:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
-  else if int % 5 == 0
+  elsif 
+  int % 5 == 0
     return "Buzz"
-    else (int % 3 == 0 && int % 5 == 0)
+    else 
+      (int % 3 == 0 && int % 5 == 0)
       return "FizzBuzz"
     end
 end
